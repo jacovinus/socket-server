720f10a4f1cf3d5f03460dd274ae568065010186
jacovinus <jacovinus@gmail.com>
2020-06-27 11:48:36 +0200

    config sockets

--- classes/server.ts
+++ classes/server.ts
@@ -1,15 +1,33 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 export default class Server {
+
 public app: express.Application;
 public port: number;
-public hostname = ''
-constructor(){
+private static  _instance: Server;
+public io: socketIO.Server;
+private httpServer : http.Server;
+private constructor(){
     this.app = express();
     this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    this.io = socketIO(this.httpServer);
+    this.listenSockets()
 }
 
-start( callback: Function) {
-    this.app.listen(this.port, callback);
+public static get instance() {
+ return this._instance || (this._instance = new this() );
+}
+start( callback:any ):void {
+    this.httpServer.listen(this.port, callback);
+}
+listenSockets(){
+    console.log('Escuchando conecciones')
+    this.io.on("connection",client => {
+        console.log('nuevo cliente conectado');
+        
+    })
 }
 }
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import Server from './classes/server';
 import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-const server = new Server();
+const server = Server.instance;
 // body parser
 server.app.use(bodyParser.urlencoded({ extended: true}));
 server.app.use( bodyParser.json());
